9e78c670df39ecfa4b78725e0b4c3cef9fdc7d73
Nikolaos Stamatopoulos <stamatopoulosnikos@gmail.com>
2019-12-27 18:58:10 +0100

    Switch to pure HTML and Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  theme: {
+    container: {
+      center: true,
+    },
+    fontFamily: {
+      fira: ['Fira Code', 'monospace'],
+    },
+    extend: {
+      width: {
+        '864': '864px',
+      },
+      colors: {
+        dark: '#1f222a',
+        accent: '#78e2a0',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+// const TerserJSPlugin = require('terser-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const devMode = process.env.NODE_ENV !== 'production';
+
+module.exports = {
+  entry: './css/main.css',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: process.env.NODE_ENV === 'development',
+            },
+          },
+          { loader: 'css-loader', options: { importLoaders: 1 } },
+          'postcss-loader',
+        ],
+      },
+    ],
+  },
+  optimization: {
+    minimizer: [new OptimizeCSSAssetsPlugin({})],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      filename: 'index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: devMode ? '[name].css' : '[name].[hash].css',
+    }),
+  ],
+};
